53c5bdfa1ee6084ae722f11c3091b0fe684098ec
erudite-colloab <eugenebaahasamoah@gmail.com>
2023-04-23 17:51:06 -0700

    functions

--- main.h
+++ main.h
@@ -3,17 +3,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+
 /* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
+
 /* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
+
 /**
 * struct fmt - Struct op
 *
@@ -87,11 +91,13 @@ int width, int flags, char padd, char extra_c, int padd_start);
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 int flags, int width, int precision, int size);
+
 /****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
+
 #endif /* MAIN_H */
 
